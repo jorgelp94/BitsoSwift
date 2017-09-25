5833363b67a9c30ff5c68b4fb42ae50a7bdc27b4
Jorge Perales <jorge@axented.com>
2017-09-24 21:53:17 -0500

    Initial Commit

---
+++ BitsoSwift/BitsoSwift.h
@@ -0,0 +1,19 @@
+//
+//  BitsoSwift.h
+//  BitsoSwift
+//
+//  Created by Jorge Luis Perales on 9/24/17.
+//  Copyright © 2017 Jorge Perales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BitsoSwift.
+FOUNDATION_EXPORT double BitsoSwiftVersionNumber;
+
+//! Project version string for BitsoSwift.
+FOUNDATION_EXPORT const unsigned char BitsoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BitsoSwift/PublicHeader.h>
+
+
